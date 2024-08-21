087a2fa84143a83b63661a96c9f705ae20ea3fb1
ealinaa <alinaadhikari108@gmail.com>
2024-08-21 21:57:46 +0545

    worked in navbar

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Button, Container, Flex, HStack, Text, useColorMode } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { PlusSquareIcon } from "@chakra-ui/icons"
+
+const Navbar = () => {
+    const { toggleColorMode, colorMode } = useColorMode()
+  return (
+    <Container maxW={"1140px"} px={4}>
+        <Flex
+        h ={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+            base:"column",
+            sm:"row"
+
+        }}
+        >
+            <Text
+            fontSize={{base: "22", sm: "28"}}
+            fontWeight={"bold"}
+            textTransform = {"uppercase"}
+            textAlign ={"center"}
+            bgGradient={'linear(to-r,cyan.200, blue.500)'}
+            bgClip={"text"}
+           
+          >
+           <Link to={"/"}>Product Store W</Link>
+  
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+            <Link to= {"/create"}>
+            <Button>
+                <PlusSquareIcon fontSize={23}/>
+            </Button>
+            </Link>
+            <Button onClick={toggleColorMode}>
+
+     {colorMode === 'light' ? 'light' : 'dark'}
+      </Button>
+
+        </HStack>
+        </Flex>
+
+    </Container>
+  )
+}
+
+export default Navbar
